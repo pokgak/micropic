9b6e524ec0374c69e1f4aac4f00aeb6aaf14fd71
pokgak <m.aimanismail@gmail.com>
2020-04-21 16:09:40 +0200

    upload file and check for validity skeleton

--- picture.js
+++ picture.js
@@ -1,15 +1,32 @@
 var express = require('express')
 var router = express.Router()
 
+var checkFile = function (req, file, cb) {
+    console.log("checking file validity")
+    cb(null, false)
+}
+
+var upload = require('multer')({ dest: 'uploads/', fileFilter: checkFile })
+
+
+const uploadDir = 'uploads/'
+
 router.get('/:uuid', function (req, res, next) {
     console.log('GET /picture uuid ' + req.params['uuid'])
+    next()
 },
-    express.static('uploaded')
+    express.static('uploads')
 )
 
-router.post('/', function (req, res) {
-    console.log("delete picture")
-    res.send("post picture")
+router.post('/', upload.single('data'), function (req, res) {
+    if (req.file === undefined) {
+        res.sendStatus(400)
+        return
+    }
+
+    console.log(req.file)
+
+    res.send("post picture " + req.file.filename)
 })
 
 router.delete('/', function (req, res) {
