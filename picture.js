8854b28614e3b50013d3e93858c9ec573b4fce47
pokgak <m.aimanismail@gmail.com>
2020-04-21 16:23:41 +0200

    return JSON and implement DELETE

--- picture.js
+++ picture.js
@@ -1,18 +1,16 @@
+var fs = require('fs')
 var express = require('express')
 var router = express.Router()
 
+const uploadDir = 'uploads/'
 var checkFile = function (req, file, cb) {
     console.log("checking file validity")
-    cb(null, false)
+    cb(null, true)
 }
+var upload = require('multer')({ dest: uploadDir, fileFilter: checkFile })
 
-var upload = require('multer')({ dest: 'uploads/', fileFilter: checkFile })
-
-
-const uploadDir = 'uploads/'
-
-router.get('/:uuid', function (req, res, next) {
-    console.log('GET /picture uuid ' + req.params['uuid'])
+router.get('/:id', function (req, res, next) {
+    console.log('GET /picture id ' + req.params['id'])
     next()
 },
     express.static('uploads')
@@ -20,18 +18,25 @@ router.get('/:uuid', function (req, res, next) {
 
 router.post('/', upload.single('data'), function (req, res) {
     if (req.file === undefined) {
-        res.sendStatus(400)
-        return
+        res.sendStatus(400).end()
     }
 
     console.log(req.file)
 
-    res.send("post picture " + req.file.filename)
+    res.json({
+        'urls': [
+            '/' + uploadDir + req.file.filename,
+        ]
+    })
 })
 
-router.delete('/', function (req, res) {
+router.delete('/:id', function (req, res) {
     console.log("delete picture")
-    res.send("delte picture")
+    fs.unlink(uploadDir + req.params['id'], (err) => {
+        if (err) throw err;
+        console.log("successfully deleted /" + uploadDir + req.params['id'])
+    })
+    res.sendStatus(204).end()
 })
 
 module.exports = router
